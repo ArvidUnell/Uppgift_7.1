b2a3ebe5721fcb2f33658e4a0b2adc6e310766e4
ArvidUnell <arun06001@utb.vaxjo.se>
2024-02-02 11:01:59 +0100

    Add project files.

---
+++ Uppgift_7.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Uppgift_7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> namnLista = new List<string>();
+
+            Console.WriteLine("Mata in så många namn du vill. Skriv en tom rad när du vill sluta");
+
+            while (true) //Matar in namn i listan
+            {
+                string nyttNamn = Console.ReadLine();
+                if (nyttNamn == "")
+                {
+                    break;
+                }
+                namnLista.Add(nyttNamn);
+            }
+
+            namnLista.Sort(); //Sorterar i alfabetisk ordning
+
+            Console.WriteLine("\nHär kommer alla namn du skrev i omvänd bokstavsordning:");
+            for (int i = 0; i < namnLista.Count; i++) //Skriver listan med det sista elementet först
+            {
+                Console.WriteLine(namnLista[namnLista.Count-1-i]);
+            }
+
+
+            Console.WriteLine("\nTryck på valfri knapp för att avsluta");
+            Console.ReadKey();
+        }
+    }
+}
